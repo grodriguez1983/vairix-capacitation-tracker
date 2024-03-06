1eb924d5a42c23da9dbf2a3439a8632c5dafeb31
Leonardo Bellene <lbellene@vairix.com>
2024-03-06 14:20:49 -0300

    office validation

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,4 +1,4 @@
-import { Office, OfficesResponse } from "@/types/offices";
+import { Office, OfficeType, OfficesResponse } from "@/types/offices";
 import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
 
@@ -46,7 +46,13 @@ export const fetchOffices = async (
       skip: ITEM_PER_PAGE * (page - 1),
     });
 
-    return { count, offices };
+    // Validate and typecast each office
+    const typedOffices: Office[] = offices.map((office) => ({
+      ...office,
+      type: office.type as OfficeType,
+    }));
+
+    return { count, offices: typedOffices };
   } catch (error) {
     console.error("Error fetching offices:", error);
     return { count: 0, offices: [], error: "Error fetching offices" };
@@ -78,7 +84,17 @@ export const fetchOffice = async (id: string): Promise<Office | null> => {
       },
     });
 
-    return office;
+    // Validate and typecast each office
+    if (office) {
+      const typedOffice: Office = {
+        ...office,
+        type: office.type as OfficeType,
+      };
+
+      return typedOffice;
+    } else {
+      return null;
+    }
   } catch (err) {
     console.log(err);
     throw new Error("Failed to fetch office!");
