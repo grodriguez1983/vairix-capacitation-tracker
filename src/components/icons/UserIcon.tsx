a433aa0cb445aabc05ea7f35120a3115ae4319c5
Baltazar Andersson <baltazar@vairix.com>
2024-03-08 13:40:30 -0300

    fix: svg console errors

--- src/components/icons/UserIcon.tsx
+++ src/components/icons/UserIcon.tsx
@@ -12,9 +12,9 @@ export const UserIcon = (props: SVGProps<SVGSVGElement>) => (
     <path
       d="M12 12a5 5 0 1 0 0-10 5 5 0 0 0 0 10ZM20.59 22c0-3.87-3.85-7-8.59-7s-8.59 3.13-8.59 7"
       stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     ></path>
   </svg>
 );
